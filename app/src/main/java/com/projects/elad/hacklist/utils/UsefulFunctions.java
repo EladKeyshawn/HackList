4f7abbe87dedb8d0402fc36bfbf24a778184b73e
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-08-14 02:07:30 +0300

    added utils class and package

---
+++ app/src/main/java/com/projects/elad/hacklist/utils/UsefulFunctions.java
@@ -0,0 +1,18 @@
+package com.projects.elad.hacklist.utils;
+
+
+public class UsefulFunctions {
+
+
+  // get month in calender format and gives a string
+  public static String getStringForMonthInt(int monthInt) {
+    int temp = monthInt + 1;
+    StringBuilder resultString = new StringBuilder();
+    if (temp > 0 && temp < 10) {
+      resultString.append("0").append(String.valueOf(temp));
+    } else {
+      resultString.append(String.valueOf(temp));
+    }
+    return resultString.toString();
+  }
+}

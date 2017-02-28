6e0f067fbc6dd28e3769218927a40bd039446ad2
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-03-01 00:32:10 +0200

    formatting

--- app/src/main/java/com/projects/elad/hacklist/presentation/main/activities/MainActivity.java
+++ app/src/main/java/com/projects/elad/hacklist/presentation/main/activities/MainActivity.java
@@ -1,12 +1,12 @@
 package com.projects.elad.hacklist.presentation.main.activities;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -45,10 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
     tabLayout = (TabLayout) findViewById(R.id.tabs);
     tabLayout.setupWithViewPager(viewPager);
-//    tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_selected);
-//    tabLayout.getTabAt(1).setIcon(R.drawable.ic_star_unselected);
-
-
 
   }
 
